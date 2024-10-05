8ee805e818149d9e110665e7be34c63e8d92d6ff
Gurpal <gillgurpal699@gmail.com>
2024-10-05 10:53:02 +0530

    remove older code

--- apps/web/constants/index.ts
+++
@@ -1,6 +0,0 @@
-export const API_URL =
-  process.env.NODE_ENV === "development"
-    ? "http://localhost:8000"
-    : "https://logstrap-backend.cantcode.fyi";
-
-export const COOKIE_NAME = "logstrap_token";
